c2009b7955685bad24409bdc17efc3378747f394
Nibbles <nibbles.cool@foxmail.com>
2022-07-25 21:19:03 +0800

    address update

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'Nibbles', // 作者
   BIO: 'Nibbles Studio创始人', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  LINK: 'https://nibbles.cool', // 网站地址
   KEYWORDS: 'Nibbles,博客,Nibbles Blog,半字节博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || 'a6eae34f87864fb4892af30bbaf73b5f', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
